17f9c1ce05cff682b63b8cdba64a8ff7b5c8207b
latmannen <163900633+latmannen@users.noreply.github.com>
2024-04-10 16:14:14 +0000

    added rolls to 100d6 page but need to do to others

--- dice.rb
+++ dice.rb
@@ -35,10 +35,9 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do 
-  first_die = rand(1..20)
-  sum = first_die
+  @die = rand(1..20)
 
-  @outcome = "You rolled a #{first_die} for a total of #{sum}."
+  @outcome = "You rolled a #{@die}."
 
     erb(:one_twenty)
 end
@@ -54,4 +53,16 @@ get("/dice/5/4") do
   @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 
     erb(:five_four)
+end 
+    
+get("/dice/100/6") do
+      @rolls = []
+    
+      100.times do
+        die = rand(1..6)
+    
+        @rolls.push(die)
+      end
+    
+      erb(:one_hundred_six)
 end
